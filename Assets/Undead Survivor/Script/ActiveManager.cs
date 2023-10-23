eb1b3b4638bb56062cec57b6be300d52ae083f22
tnt-exe <119075262+tnt-exe@users.noreply.github.com>
2023-10-23 13:32:23 +0700

    feat: implement character unlock with notice

---
+++ Assets/Undead Survivor/Script/ActiveManager.cs
@@ -0,0 +1,95 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class ActiveManager : MonoBehaviour
+{
+    public GameObject[] lockCharacters;
+    public GameObject[] unlockCharacters;
+    public GameObject uiNotice;
+
+    enum Achive { UnlockPotato, UnlockBean }
+    Achive[] achives;
+    WaitForSecondsRealtime wait;
+
+    void Awake()
+    {
+        achives = Enum.GetValues(typeof(Achive)) as Achive[];
+        wait = new WaitForSecondsRealtime(5);
+
+        if (!PlayerPrefs.HasKey("MyData"))
+        {
+            Init();
+        }
+    }
+
+    void Init()
+    {
+        PlayerPrefs.SetInt("MyData", 1);
+
+        foreach (Achive achive in achives)
+        {
+            PlayerPrefs.SetInt(achive.ToString(), 0);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UnlockCharacter();
+    }
+
+    void UnlockCharacter()
+    {
+        for (int index = 0; index < lockCharacters.Length; index++)
+        {
+            string achiveName = achives[index].ToString();
+            bool isUnlock = PlayerPrefs.GetInt(achiveName) == 1;
+            lockCharacters[index].SetActive(!isUnlock);
+            unlockCharacters[index].SetActive(isUnlock);
+        }
+    }
+
+    void LateUpdate()
+    {
+        foreach (Achive achive in achives)
+        {
+            CheckActive(achive);
+        }
+    }
+
+    void CheckActive(Achive achive)
+    {
+        bool isAchive = false;
+
+        switch (achive)
+        {
+            case Achive.UnlockPotato:
+                isAchive = GameManager.instance.kill >= 10;
+                break;
+            case Achive.UnlockBean:
+                isAchive = GameManager.instance.gameTime == GameManager.instance.maxGameTime;
+                break;
+        }
+
+        if (isAchive && PlayerPrefs.GetInt(achive.ToString()) == 0)
+        {
+            PlayerPrefs.SetInt(achive.ToString(), 1);
+
+            for (int index = 0; index < uiNotice.transform.childCount; index++)
+            {
+                bool isActive = index == (int)achive;
+                uiNotice.transform.GetChild(index).gameObject.SetActive(isActive);
+            }
+
+            StartCoroutine(NoticeRoutine());
+        }
+    }
+
+    IEnumerator NoticeRoutine()
+    {
+        uiNotice.SetActive(true);
+        yield return wait;
+        uiNotice.SetActive(false);
+    }
+}

258a09db247fae10b330af2c96382718960461fa
tnt-exe <119075262+tnt-exe@users.noreply.github.com>
2023-10-26 05:12:30 +0700

    feat: add 3 new enemies and implement enemy notice

--- Assets/Undead Survivor/Script/Spawner.cs
+++ Assets/Undead Survivor/Script/Spawner.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
@@ -6,13 +7,16 @@ public class Spawner : MonoBehaviour
     public SpawnData[] spawnData;
     public float levelTime;
 
+    public GameObject uiEnemyNotice;
+
     float timer;
-    int level;
+    int spawnLevel;
+    int enemyLevel;
 
     void Awake()
     {
         spawnPoint = GetComponentsInChildren<Transform>();
-        levelTime = GameManager.instance.maxGameTime / spawnData.Length;
+        levelTime = GameManager.instance.maxGameTime / (spawnData.Length * 1.5f);
     }
 
     // Update is called once per frame
@@ -22,20 +26,38 @@ public class Spawner : MonoBehaviour
             return;
 
         timer += Time.deltaTime;
-        level = Mathf.Min(Mathf.FloorToInt(GameManager.instance.gameTime / levelTime), spawnData.Length - 1);
+        spawnLevel = Mathf.Min(Mathf.FloorToInt(GameManager.instance.gameTime / levelTime), spawnData.Length - 1);
 
-        if (timer > spawnData[level].spawnTime)
+        if (timer > spawnData[spawnLevel].spawnTime)
         {
             timer = 0;
             Spawn();
         }
     }
 
+    void LateUpdate()
+    {
+        if (spawnLevel != 0 && enemyLevel != spawnLevel)
+        {
+            enemyLevel = spawnLevel;
+            StartCoroutine(EnemyNoticeRoutine());
+        }
+    }
+
+    IEnumerator EnemyNoticeRoutine()
+    {
+        uiEnemyNotice.gameObject.SetActive(true);
+        uiEnemyNotice.transform.GetChild(enemyLevel).gameObject.SetActive(true);
+
+        yield return new WaitForSecondsRealtime(5);
+        uiEnemyNotice.gameObject.SetActive(false);
+    }
+
     void Spawn()
     {
         GameObject enemy = GameManager.instance.pool.Get(0);
         enemy.transform.position = spawnPoint[Random.Range(1, spawnPoint.Length)].position;
-        enemy.GetComponent<Enemy>().Init(spawnData[level]);
+        enemy.GetComponent<Enemy>().Init(spawnData[spawnLevel]);
     }
 }
 

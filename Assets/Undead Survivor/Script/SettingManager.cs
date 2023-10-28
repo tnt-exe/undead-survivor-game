2fb0644c30beca2ebc6841481392efc9a6b63577
tnt-exe <119075262+tnt-exe@users.noreply.github.com>
2023-10-28 13:38:10 +0700

    fix: remove settings, change to restart in game menu

--- Assets/Undead Survivor/Script/SettingManager.cs
+++ Assets/Undead Survivor/Script/SettingManager.cs
@@ -4,37 +4,6 @@ public class SettingManager : MonoBehaviour
 {
     public static SettingManager instance;
 
-    public float volume;
-    [Header("# BGM")]
-    public bool bgmOff;
-
-    [Header("# SFX")]
-    public bool sfxOff;
-
-    void Awake()
-    {
-        if (instance == null)
-        {
-            instance = this;
-        }
-    }
-
-    public void muteBgm()
-    {
-        bgmOff = true;
-        AudioManager.instance.PlayBgm(!bgmOff);
-    }
-
-    public void muteSfx()
-    {
-        sfxOff = true;
-    }
-
-    public void changeVol(float vol)
-    {
-
-    }
-
     public void clearPlayerPrefs()
     {
         PlayerPrefs.DeleteAll();
